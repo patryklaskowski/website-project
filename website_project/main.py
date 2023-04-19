c7e262e36968e8e003760e453592702a4261adf5
Patryk Laskowski <patryk.jacek.laskowski@gmail.com>
2023-04-19 18:24:43 +0200

    test

--- website_project/main.py
+++ website_project/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World Test Test Test!</p>"
+    return "<p>Hello, World Test Test Test! test-build-and-publish-docker-image-action-01</p>"
 
 
 if __name__ == "__main__":
