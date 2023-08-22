3a8e7f3190e5eb9abf2d98bf23c72e1aa4282728
Patryk Laskowski <98971931+laskowp1@users.noreply.github.com>
2023-08-22 18:31:55 +0200

    add base html template with navbar and footer (#17)

--- tests/test_welcome_page.py
+++ tests/test_welcome_page.py
@@ -2,8 +2,7 @@ from flask.testing import FlaskClient
 
 
 def test_welcome_page(flask_app: FlaskClient):
-    response = flask_app.get()
+    response = flask_app.get("/")
 
     assert response.status_code == 200
     assert "<h1>Welcome to 'Website-Project'</h1>" in response.text
-
--- website_project/main.py
+++ website_project/main.py
@@ -1,35 +1,11 @@
-import logging
 from flask import Flask, render_template
-from flask_restful import Resource, Api
-from typing import NewType
 
 app = Flask(__name__)
-api = Api(app)
-
-LOG = logging.getLogger(__name__)
-
-API_PREFIX = "restapi"
-
-HTML = NewType("html", str)
-JSON = NewType("json", dict)
 
 
 @app.route("/", methods=['GET'])
-def welcome_page() -> HTML:
+def home_page() -> str:
     return render_template("welcome.html")
-
-
-class HealthResource(Resource):
-    @staticmethod
-    def get() -> JSON:
-        return {"message": "ok"}
-
-
-api.add_resource(
-    HealthResource,
-    f"/{API_PREFIX}/health",
-    f"/{API_PREFIX}/",
-)
 
 
 if __name__ == "__main__":
