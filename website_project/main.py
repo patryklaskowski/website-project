9aab2d6274c971f0d9c45016f68b701a93ab08ac
Patryk Laskowski <98971931+laskowp1@users.noreply.github.com>
2023-08-23 18:38:22 +0200

    add base activity form for swimming and running (#18)

--- website_project/main.py
+++ website_project/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def home_page() -> str:
     return render_template("welcome.html")
+
+
+@app.route("/activity", methods=['GET', 'POST'])
+def activity_form() -> str:
+    if request.method == 'GET':
+        return render_template("activity_form.html")
+
+    print(f"POST request form: `{request.form}`")
+    return render_template("activity_form.html")
 
 
 if __name__ == "__main__":
