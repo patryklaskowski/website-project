f86f4f39e090b4ffcae96bc5e4a68df4e25d11cc
laskowp1 <patryk.laskowski@contractors.roche.com>
2023-09-10 00:14:49 +0200

    apply to main

--- website_project/main.py
+++ website_project/main.py
@@ -1,15 +1,21 @@
 from dataclasses import dataclass
 from typing import List
+from datetime import timedelta
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, session
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask import Response, Flask, render_template, request, redirect, url_for, session
+from flask_login import current_user, login_required, login_user, logout_user
 
 from website_project.activity import read_activity_specific_data, activities
-from website_project.common import get_config
+from website_project.common import get_config, Alert, AlertType, link_to
 from website_project.database.driver import MongoDb
-from website_project.models import User, registered_users
+from website_project.login_manager import create_login_manager
+from website_project.user import (
+    RegisteredUser,
+    find_user_by_username,
+    register_user,
+    user_exists,
+)
 
 load_dotenv()
 
@@ -19,43 +25,45 @@ db = MongoDb(
     username=config["mongodb"]["username"],
 )
 
-from flask_login.mixins import AnonymousUserMixin
-class Anonymous(AnonymousUserMixin):
-  def __init__(self):
-    self.name = 'Guest'
+
+class Cookie:
+    """Support for session values."""
+    ALERT = "my_alert"
+    REDIRECT_BACK = "my_redirect_back"
 
 
+def create_app() -> Flask:
+    """Handles Flask setup."""
+    flask_app = Flask(__name__)
+    flask_app.secret_key = "secret"
 
-app = Flask(__name__)
-app.secret_key = "secret"
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
-login_manager.anonymous_user = Anonymous
-
-registered_users.append(User("root@gmail.com", generate_password_hash("root", "sha256"), "root"))
-
-@login_manager.user_loader
-def load_user(user_id):
-    user = None
-    if registered_users:
-        for u in registered_users:
-            if u.id == user_id:
-                user = u
-    return user
+    return flask_app
 
 
-@dataclass
-class Alert:
-    """Support data type for html alerts."""
-    type: str
-    msg: str
+app = create_app()
+login_manager = create_login_manager(app)
 
 
-class AlertType:
-    SUCCESS = "success"
-    ERROR = "danger"
-    INFO = "info"
+@login_manager.unauthorized_handler
+def unauthorized_access_view() -> Response:
+    """Handles unauthorized situation.
+
+    When guest user tries to access website for logged-in users only.
+
+    Note:
+        Remember, that not only website buttons direct to specific web page,
+        is might also be typed in by hand. Guard against unauthorized access
+        with this generic solver.
+    """
+    print(f"You need to be logged in to enter {url_for(request.endpoint)} endpoint.")
+    session[Cookie.ALERT] = Alert(
+        AlertType.WARNING,
+        f"You need to be logged in to access {link_to(request.endpoint)} endpoint.",
+    )
+
+    session[Cookie.REDIRECT_BACK] = request.endpoint
+
+    return redirect(url_for('login'))
 
 
 @dataclass
@@ -66,6 +74,7 @@ class DataTable:
     rows: List[List[str]]
 
 
+# TODO: This is either queried all at once by selected activity
 def get_all_data() -> List[DataTable]:
     """Retrieves all data for all activities."""
     all_data = []
@@ -82,71 +91,81 @@ def get_all_data() -> List[DataTable]:
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    """Login page."""
     if request.method == "GET":
-        return render_template("login.html", alert=session.pop("alert", None))
+        return render_template("login.html", alert=session.pop(Cookie.ALERT, None))
     elif request.method == "POST":
-        form = request.form
+        username = request.form["username"]
+        password = request.form["password"]
 
-        user = None
-        for u in registered_users:
-            user = u if form["email"] == u.email else None
+        user = find_user_by_username(username)
+        if not user or not user.has_password(password):
+            print("Login failed, provided invalid username or password.")
+            session[Cookie.ALERT] = Alert(AlertType.ERROR, f"Invalid username or password.")
 
-        if not user or not check_password_hash(user.password, form["password"]):
-            session["alert"] = Alert(AlertType.ERROR, f"Invalid username or password.")
             return redirect(url_for('login'))
 
-        session["alert"] = Alert(AlertType.INFO, f"Welcome, {user.name}")
-        login_user(user)
+        print(f"Login successful for {user}")
+        session[Cookie.ALERT] = Alert(AlertType.INFO, f"Successful login, {user.username}")
+        login_user(user, remember=True, duration=timedelta(weeks=1))
+
+        if session.get(Cookie.REDIRECT_BACK, None):
+            return redirect(url_for(session.pop(Cookie.REDIRECT_BACK)))
+
         return redirect(url_for('profile'))
 
 
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
     if request.method == "GET":
-        return render_template("signup.html", alert=session.pop("alert", None))
+        return render_template("signup.html", alert=session.pop(Cookie.ALERT, None))
     elif request.method == "POST":
-        form = request.form
+        username = request.form["username"]
+        password = request.form["password"]
 
-        user = User(
-            email=form["email"],
-            password=generate_password_hash(form["password"], method="sha256"),
-            name=form["name"],
-        )
+        user = RegisteredUser(username, password)
 
-        # Check if user already exists
-        if user.email in [u.email for u in registered_users]:
+        if user_exists(user):
             return render_template(
                 "signup.html",
                 alert=Alert(
                     AlertType.ERROR,
-                    f"There's account connected to provided email: `{user.email}`.",
+                    f"Username `{user.username}` already in use.",
                 )
             )
 
-        # Add user
-        registered_users.append(user)
-        print(f"Registered new user: {user}")
-        session["alert"] = Alert(AlertType.SUCCESS, f"User has been created with email: `{user.email}`")
+        register_user(user)
+        print(f"Registered new user: {user.username}")
+        session[Cookie.ALERT] = Alert(AlertType.SUCCESS, f"New user has been created: {user.username}")
+
+        if not current_user.is_authenticated:
+            login_user(user, remember=True, duration=timedelta(weeks=1))
 
         return redirect(url_for('login'))
 
+
 @app.route('/logout')
 def logout():
-    logout_user()
-    session["alert"] = Alert(AlertType.INFO, f"User logged out.")
+    if current_user.is_authenticated:
+        logout_user()
+        session[Cookie.ALERT] = Alert(AlertType.INFO, f"User logged out.")
+
     return redirect(url_for('login'))
+
 
 @app.route('/profile')
 def profile():
-    # alert = session.pop("alert") if "alert" in session else None
-    return render_template('profile.html', alert=session.pop("alert", None), name=current_user.name)
+    return render_template(
+        'profile.html',
+        alert=session.pop(Cookie.ALERT, None),
+        name=current_user.username
+    )
 
 
 @app.route("/", methods=['GET'])
 def home_page() -> str:
     """Website home page."""
-    # alert = session.pop("alert") if "alert" in session else None
-    return render_template("welcome.html", alert=session.pop("alert", None))
+    return render_template("welcome.html", alert=session.pop(Cookie.ALERT, None))
 
 
 @app.route("/activity", methods=['GET', 'POST'])
@@ -159,7 +178,7 @@ def activity_form() -> str:
         and saved in database.
     """
     if request.method == "GET":
-        return render_template("activity_form.html")
+        return render_template("activity_form.html", alert=session.pop(Cookie.ALERT, None))
     elif request.method == "POST":
         form_data = request.form.to_dict()
         print(f"POST request form: `{form_data}`")
@@ -181,11 +200,12 @@ def activity_form() -> str:
 @login_required
 def activity_data() -> str:
     """Present registered activity data."""
-    return render_template("activity_data.html", all_data=get_all_data())
+    return render_template("activity_data.html", all_data=get_all_data(), alert=session.pop(Cookie.ALERT, None))
 
 
 if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=5000,
+        threaded=True,
     )
